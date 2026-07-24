b486af799deb2414def4158afdc38bdc7e425180
Vishnu_Mac <thenormvg@gmail.com>
2026-07-25 03:37:47 +0530

    docs: add copy page markdown button component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import CopyPageMarkdown from './components/CopyPageMarkdown.vue'
 import './style.css'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => h(CopyPageMarkdown)
+    })
+  }
 }
